6ae141c0fac28fd113262aadc28876243cf422fa
Sean Tan <sean.tan@mail.mcgill.ca>
2021-11-15 20:38:50 -0500

    scanning

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -25,3 +25,6 @@ export const randomIntFromInterval = (min, max) => { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
+export const sleep = (milliseconds) => {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
